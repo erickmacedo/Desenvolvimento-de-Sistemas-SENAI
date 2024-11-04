00cf927c7c4aa05fae26728cc38a0263ec41280b
Erick Macedo <32196382+erickmacedo@users.noreply.github.com>
2024-11-04 20:34:43 -0300

    Add files via upload

--- API/models/Pessoa.js
+++ API/models/Pessoa.js
@@ -1,1 +1,26 @@
+const sequelize = require('../database')
+const { DataTypes} = require('sequelize')
 
+const Pessoa = sequelize.define('Pessoa',{
+    nome:{
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    idade:{
+        type: DataTypes.INTEGER,
+        allowNull:false
+    },
+    descricao:{
+        type: DataTypes.STRING
+    },
+    avatar:{
+        type: DataTypes.TEXT,
+        defaultValue:''
+    }
+})
+
+sequelize.sync()
+    .then(()=> console.log("Tabela de pessoas sincronizada"))
+    .catch(err => console.log("Erro ao sincronizar tabela produtos: ", err))
+
+module.exports = Pessoa
